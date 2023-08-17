791493b13c0112fbf1f46720bfb0a23c59c82209
Hayden Lau <hayden@deltaepsilon.ca>
2023-08-17 13:45:09 -0400

    output dir argument, delete first

--- src/canva_site_ripper/rip.py
+++ src/canva_site_ripper/rip.py
@@ -1,15 +1,32 @@
 import argparse
 import pathlib
+import shutil
 import urllib.parse
 
 import requests
 
 from canva_site_ripper import clean
 
+def dir_path(argstr):
+    try:
+        path = pathlib.Path(argstr)
+        try:
+            shutil.rmtree(path)
+        except FileNotFoundError:
+            pass
+    except Exception as err:
+        raise argparse.ArgumentTypeError("can't delete existing directory: " + str(err)) from err
+    try:
+        path.mkdir()
+    except Exception as err:
+        raise argparse.ArgumentTypeError("can't create output directory: " + str(err)) from err
+    return path
+
 def parse_args():
     parser = argparse.ArgumentParser("canva-site-ripper", description="Tool for ripping and cleaning Canva websites")
     parser.add_argument("canva_url", help="URL of the Canva site.", type=urllib.parse.urlparse)
     parser.add_argument("new_url", help="URL that the site will be hosted on.", type=urllib.parse.urlparse)
+    parser.add_argument("output_dir", help="Directory to save the website files. Existing files will be deleted.", type=dir_path)
     parser.add_argument("-f", "--file", help="Use this HTML file instead of download directly from Canva.", type=argparse.FileType("r"))
     parser.add_argument("-r", "--no-robots", action="store_true", help="Add a robots.txt disallowing all crawlers to the output")
     return parser.parse_args()
@@ -23,7 +40,8 @@ def main():
         html = requests.get(urllib.parse.urlunparse(args.canva_url)).text
     cleaned_html, to_download = clean.clean(html, args.canva_url, args.new_url)
 
-    # TODO: search for "canva" in the cleaned HTML
+    if "canva" in cleaned_html.lower():
+        print("Warning: string 'canva' found in cleaned HTML")
 
     downloaded = {}
     for file_to_download in to_download:
@@ -31,7 +49,6 @@ def main():
         response = requests.get(download_url)
         downloaded[file_to_download] = response.content
 
-    # TODO: delete existing
     basedir = pathlib.Path("cleaned_website")
     basedir.mkdir(exist_ok=True)
     with (basedir / "index.html").open("w") as index_file:
